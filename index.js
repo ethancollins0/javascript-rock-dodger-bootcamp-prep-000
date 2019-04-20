c960675b97097582678e7cdc710c1b90b8f6a2b2
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 22:13:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ window.requestAnimationFrame(moveRock)
 function endGame() {
   clearInterval(gameInterval);
   ROCKS = []
-  document.getElementsByClassName('rock')
+  $('rock').remove()
   window.removeEventListener('keydown', moveDodger)
   alert('YOU LOSE!')
 }
