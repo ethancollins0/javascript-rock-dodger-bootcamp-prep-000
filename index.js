0e942ba6b49ddb0a12486c4b21d17e65e518a417
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 22:43:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -117,7 +117,6 @@ window.requestAnimationFrame(moveRock)
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  alert('endgame inner')
   clearInterval(gameInterval);
   $('rock').remove()
   ROCKS = []
