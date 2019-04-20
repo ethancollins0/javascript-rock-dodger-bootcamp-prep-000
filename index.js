0129faf7a71faccdc918cba4fadbebb0b46860d2
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 23:36:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -109,6 +109,10 @@ function endGame() {
 }
 
 function moveDodger(e) {
+  if (e.which !== LEFT_ARROW || e.which !== RIGHT_ARROW) {
+    e.stopPropagation()
+    e.preventDefault()
+  } 
   if (e.which === LEFT_ARROW) {
     moveDodgerLeft()
   }
