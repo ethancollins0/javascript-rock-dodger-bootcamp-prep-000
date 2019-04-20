49a41a6b8f8584880dee6fce3b8f85e3e5eac7cf
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 20:17:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function createRock(x) {
    */
   function moveRock() {
     function step() {
-      rock.style.top = `${top += 2}px`
+      rock.style.top = += 2
 
       if (top < 200) {
         window.requestAnimationFrame(step)
