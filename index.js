5ef4a39408e8cfc834cb65e14fcf2923e6595278
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 19:42:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function moveDodger(e) {
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
- 
+
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
