2150e6dc423c3e199a5bc1fcbaf5da5d0f7bf9dc
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 21:36:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ window.requestAnimationFrame(moveRock)
         endGame()
       }
       if (rock[i].style.top < 100) {
-        rock[i].GAME.removeChild(rock[i])
+        document.getElementById('game').removeChild(rock[i])
       }
 
     }
