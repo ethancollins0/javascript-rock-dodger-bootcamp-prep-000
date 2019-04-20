3500c0b9def15135083ea083704ea540ab7093fe
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 22:25:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -117,6 +117,7 @@ window.requestAnimationFrame(moveRock)
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
+    clearInterval(gameInterval);
   window.requestAnimationFrame($('rock').remove())
   window.removeEventListener('keydown', moveDodger)
   alert('YOU LOSE!')
