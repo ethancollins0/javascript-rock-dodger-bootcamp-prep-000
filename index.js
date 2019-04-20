84c8bf77e26b220f13ae61d6aca2a87e8dc74768
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 19:49:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -118,14 +118,6 @@ function moveDodger(e) {
   if (e.key === RIGHT_ARROW) {
     window.requestAnimationFrame(moveDodgerRight)
   }
-  // implement me!
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
 }
 
 function moveDodgerLeft() {
