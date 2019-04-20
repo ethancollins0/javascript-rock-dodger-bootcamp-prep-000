79db4e43e3b311d7c0293d3b48a91d153e91fe17
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 20:42:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -77,9 +77,10 @@ window.requestAnimationFrame(moveRock)
    */
   function moveRock() {
     var rock = document.getElementsByClassName('rock')
+    var top = 0
       for (i = 0; i < rock.length; i++) {
         rock[i].style.top.replace('px', '')
-        var top = parseInt(rockTop, 10)
+        v = parseInt(rockTop, 10)
         rock[i].style.top += 2
       }
     /* var i = 0 {
