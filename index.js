19136b9dc4e699ccbc9523c69b9870797dab3ef5
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 22:54:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ function endGame() {
   console.log('interval cleared')
   var rock = document.getElementsByClassName('rock')
   while (rock.length > 0) {
-    
+
   }
   console.log('rocks removed')
   window.removeEventListener('keydown', moveDodger)
