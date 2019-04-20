70b7d9796108fa088a924b1ceff5c77387403027
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 23:45:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -100,9 +100,7 @@ window.requestAnimationFrame(moveRock)
 function endGame() {
   clearInterval(gameInterval)
   var rock = document.getElementsByClassName('rock')
-  while (rock.length > 0) {
-    rock[0].parentNode.removeChild(rock[0])
-  }
+  $rock.remove()
   window.removeEventListener('keydown', moveDodger)
   console.log('dodger movement listener removed')
   alert('YOU LOSE!')
