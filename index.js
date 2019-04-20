41d76b10807dbdd939eea6c61d54c153274ed575
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 19:56:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function checkCollision(rock) {
 }
 
 function createRock(x) {
-  const rock = document.createElement('div')
+  var rock = document.createElement('div')
 
   rock.className = 'rock'
   rock.style.left = `${x}px`
