ac90fdd87d43646cdc904ff990632b5436cca9fa
Ethan Collins <ethantcollins98@gmail.com>
2019-04-20 20:33:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function createRock(x) {
   rock.style.top = top
 
   GAME.append(rock)
-  setInterval(window.requestAnimationFrame(moveRock), 1000)
+window.requestAnimationFrame(moveRock)
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
