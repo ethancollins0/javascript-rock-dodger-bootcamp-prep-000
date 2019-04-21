019b6111c0b471071cc4517c4b568782213f0cb9
Ethan Collins <ethantcollins98@gmail.com>
2019-04-21 00:36:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ function endGame() {
     rock[0].parentNode.removeChild(rock[0])
     ROCKS.pop()
   }
-  for (i = 0; i < rock.length; i++) {
+  for (i = rock.length; i < rock.length; i++) {
   window.requestAnimationFrame(removeRocks)
 }
   window.removeEventListener('keydown', moveDodger)
