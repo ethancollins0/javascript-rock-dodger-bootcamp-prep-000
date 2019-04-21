8b1f6537d4771a01fc289d8cac95c723a8976c24
Ethan Collins <ethantcollins98@gmail.com>
2019-04-21 00:15:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function endGame() {
   clearInterval(gameInterval)
   var rock = document.getElementsByClassName('rock')
   while (rock.length > 0) {
-    setTimeout(rock[0].parentNode.removeChild(rock[0]), 500)
+    setTimeout((rock[0].parentNode.removeChild(rock[0])), 500)
   }
   window.removeEventListener('keydown', moveDodger)
   alert('YOU LOSE!')
