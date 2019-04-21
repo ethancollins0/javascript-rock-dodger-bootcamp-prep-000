0907d78347d8e7be23770dafebc48cfa1881e3ee
Ethan Collins <ethantcollins98@gmail.com>
2019-04-21 01:35:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,11 +71,11 @@ function createRock(x) {
       if (positionToInteger(rock[i].style.top) > 395) {
         rock[i].remove()
         ROCKS.splice(i, 1)
+        window.requestAnimationFrame(moveRock);
       } else {
         rockTop = positionToInteger(rock[i].style.top)
         rock[i].style.top = `${rockTop + 20}px`
       }
-      window.requestAnimationFrame(moveRock);
 
     }
 
