a05186e5a8f090663b7d99158fdd0cae3aa009d5
Ethan Collins <ethantcollins98@gmail.com>
2019-04-21 00:32:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -107,6 +107,7 @@ function endGame() {
     rock[0].parentNode.removeChild(rock[0])
     ROCKS.pop()
   }}
+  requestAnimationFrame(removeRocks)
 
   window.removeEventListener('keydown', moveDodger)
   alert('YOU LOSE!')
