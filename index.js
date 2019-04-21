181863d3859d4c80d201a0671de9c0b5f5be102e
Ethan Collins <ethantcollins98@gmail.com>
2019-04-21 01:21:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ return rock
       }
       if (positionToInteger(rock[i].style.top) > 400) {
         rock[i].remove()
+        ROCKS.splice(i, 1)
       } else {
         rockTop = positionToInteger(rock[i].style.top)
         rock[i].style.top = `${rockTop + 74}px`
